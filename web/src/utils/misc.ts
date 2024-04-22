de8535b4c4428f1bf215b23f117692236972b1ca
Jellyton <jellyton69@gmail.com>
2024-04-22 02:32:09 -0700

    chore(ui): remove unused util function

--- web/src/utils/misc.ts
+++ web/src/utils/misc.ts
@@ -5,11 +5,6 @@ export const isEnvBrowser = (): boolean => !(window as any).invokeNative;
 // Basic no operation function
 export const noop = () => {};
 
-export function commaValue(x: any) {
-	if (x === undefined) return;
-	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
 export function formatMoney(amount: number) {
 	return amount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
