42146f0e5abd7b55a91b7d5231037be82c56b3d0
Jellyton <jellyton69@gmail.com>
2024-04-22 02:28:12 -0700

    tweak: update system font to Inter

--- web/src/providers/ThemeProvider.tsx
+++ web/src/providers/ThemeProvider.tsx
@@ -5,6 +5,7 @@ export default function ThemeProvider({ children }) {
 		<MantineProvider
 			defaultColorScheme="dark"
 			theme={{
+				fontFamily: "Inter, sans-serif",
 				colors: {
 					dark: ["#C1C2C5", "#A6A7AB", "#909296", "#5c5f66", "#373A40", "#2C2E33", "#25262b", "#1A1B1E", "#141517", "#101113"],
 				},
