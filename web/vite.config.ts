20ce50d94638940bd7a7dd1ed20ced04ea667625
Jellyton <jellyton69@gmail.com>
2024-09-29 18:38:29 -0700

    chore(ui): rollup chunking

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,14 +3,38 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: 'build',
-  },
+	plugins: [react()],
+	base: "./",
+	build: {
+		outDir: "build",
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules/.pnpm/")) {
+						// Extracting the module name for PNPM structure
+						const splitId = id.split("node_modules/.pnpm/")[1];
+						const moduleName = splitId.includes("/") ? splitId.split("/")[0] : splitId;
+
+						// Define partial names to target for separate chunking
+						const targetedPartials = ["lodash", "moment", "fortawesome", "mantine", "tabler"];
+
+						// Find the first partial that matches the module name
+						const matchingPartial = targetedPartials.find((partial) => moduleName.includes(partial));
+
+						if (matchingPartial) {
+							// Return the partial name as the chunk name
+							return matchingPartial;
+						}
+						// Grouping other dependencies into a common chunk
+						return "vendor";
+					}
+				},
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
